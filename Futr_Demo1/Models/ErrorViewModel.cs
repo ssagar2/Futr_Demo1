7b01eda735341f84380924bf5ea43b8a6f6408f7
swapnil sagar <ssagar2@uic.edu>
2020-05-29 12:08:48 -0500

    Add project files.

---
+++ Futr_Demo1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Futr_Demo1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
